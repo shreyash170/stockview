8a3e49f98211a0949a756ddd71be8279250bf41b
shreyash170 <shreyashgupta.170@gmail.com>
2024-03-19 22:46:16 +0530

    Data and search UI done

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
-import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/components/useColorScheme';
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+import React from "react";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { Link, Tabs, router } from "expo-router";
+import { Pressable } from "react-native";
+import Colors from "@/constants/Colors";
+import { useColorScheme } from "@/components/useColorScheme";
+import { useClientOnlyValue } from "@/components/useClientOnlyValue";
+import { TextInput } from "react-native-paper";
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof MaterialIcons>['name'];
+  name: React.ComponentProps<typeof MaterialIcons>["name"];
   color: string;
 }) {
   return <MaterialIcons size={28} style={{ marginBottom: -3 }} {...props} />;
@@ -20,23 +21,38 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         // Disable the static render of the header on web
         // to prevent a hydration error in React Navigation v6.
         headerShown: useClientOnlyValue(false, true),
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: "Home",
           tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
+          header: () => (
+            <Pressable
+              style={{ width: "100%", paddingHorizontal: 20, paddingTop: 50 }}
+              onPress={() => router.push("/search")}
+            >
+              <TextInput
+              placeholder="Search Stocks..."
+              disabled
+              left={<TextInput.Icon icon = {"magnify"} />}
+              onPressIn={() => router.push("/search")} />
+            </Pressable>
+          ),
         }}
       />
       <Tabs.Screen
         name="watchlist"
         options={{
-          title: 'Watchlist',
-          tabBarIcon: ({ color }) => <TabBarIcon name="bookmark-outline" color={color} />,
+          title: "Watchlist",
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="bookmark-outline" color={color} />
+          ),
         }}
       />
     </Tabs>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,32 +1,58 @@
-import { StyleSheet, View, Pressable} from 'react-native';
-import { Text } from 'react-native-paper';
-import {router} from 'expo-router';
+import { StyleSheet, View, Pressable, FlatList } from "react-native";
+import { Text } from "react-native-paper";
+import { router } from "expo-router";
+import { stocks } from "@/data";
+import { Image } from "expo-image";
 
-
-export default function TabOneScreen() {
+export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Pressable onPress={() => router.push("/Tata")}>
-        <Text>Press Me</Text>
-      </Pressable>
-
+    <View style={styles.homeContainer}>
+      <Text variant="titleLarge" style={styles.titleText}>
+        Available Stocks
+      </Text>
+      <FlatList
+        keyExtractor={(item) => item.ticker}
+        data={stocks}
+        renderItem={({ item }) => (
+          <Pressable
+            style={styles.companyDetails}
+            onPress={() => router.push(`/${item.ticker}`)}
+          >
+            <Image
+              source={item.image}
+              style={styles.companyLogo}
+              contentFit="contain"
+            />
+            <Text>{item.ticker}</Text>
+            <Text>{item.companyName}</Text>
+            <Text>{item.price}</Text>
+            <Text>
+              {item.priceChange} {item.priceChangePercentage}
+            </Text>
+          </Pressable>
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  homeContainer: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 30,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  titleText: {
+    fontWeight: "bold",
+    marginLeft: 5,
+    marginBottom: 5,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  companyLogo: {
+    height: 50,
+    width: 50,
+  },
+  companyDetails: {
+    flexDirection: "row",
+    marginVertical: 10,
+    paddingHorizontal: 10,
   },
 });
