53ec0fc14dac5af051b9239ca28c9d889fe97b5c
shreyash170 <shreyashgupta.170@gmail.com>
2024-03-21 20:45:48 +0530

    Stocks design fixed

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,17 @@
-import { StyleSheet, View, Pressable, FlatList } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Pressable,
+  FlatList,
+  useWindowDimensions,
+} from "react-native";
 import { Text } from "react-native-paper";
 import { router } from "expo-router";
 import { stocks } from "@/data";
 import { Image } from "expo-image";
 
 export default function HomeScreen() {
+  const {width} = useWindowDimensions();
   return (
     <View style={styles.homeContainer}>
       <Text variant="titleLarge" style={styles.titleText}>
@@ -23,12 +30,18 @@ export default function HomeScreen() {
               style={styles.companyLogo}
               contentFit="contain"
             />
-            <Text>{item.ticker}</Text>
-            <Text>{item.companyName}</Text>
-            <Text>{item.price}</Text>
-            <Text>
-              {item.priceChange} {item.priceChangePercentage}
-            </Text>
+            <View style={{flexDirection: 'row', justifyContent:'space-between',width: width - 75, paddingLeft: 15}}>
+              <View>
+                <Text variant="titleMedium" style={styles.textFontWeight}>{item.ticker}</Text>
+                <Text>{item.companyName}</Text>
+              </View>
+              <View>
+                <Text variant="titleMedium" style={styles.textFontWeight}>{item.price}</Text>
+                <Text>
+                  {item.priceChange} {item.priceChangePercentage}
+                </Text>
+              </View>
+            </View>
           </Pressable>
         )}
       />
@@ -54,5 +67,9 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     marginVertical: 10,
     paddingHorizontal: 10,
+    height: 60,
   },
+  textFontWeight:{
+    fontWeight: "bold"
+  }
 });
